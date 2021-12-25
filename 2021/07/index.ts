264e2988064de66c2f55f5c7ce849c8b0ff0016a
Travis Johnson <travis@thisguy.codes>
2021-12-25 05:28:04 +0000

    day 7 part 2

--- 2021/07/index.ts
+++ 2021/07/index.ts
@@ -9,15 +9,28 @@ class Crab {
 
     constructor(pos: number) {
         this.pos = pos;
-    }
+    };
 
     distanceTo(pos: number): number {
         return Math.abs(pos - this.pos);
+    };
+
+    fuelTo(pos: number): number {
+        const distance = this.distanceTo(pos);
+        let fuel = 0;
+        for (let move=1; move <= distance; move++) {
+            fuel += move;
+        };
+
+        return fuel;
     }
 }
 
 const totalFuel = function(crabs: Array<Crab>, pos: number): number {
     return crabs.map(crab => crab.distanceTo(pos)).reduce((left, right) => left + right)
+}
+const totalFuel2 = function(crabs: Array<Crab>, pos: number): number {
+    return crabs.map(crab => crab.fuelTo(pos)).reduce((left, right) => left + right)
 }
 
 export async function first(data: AsyncIterable<string>) {
@@ -37,5 +50,17 @@ export async function first(data: AsyncIterable<string>) {
 }
 
 export async function second(data: AsyncIterable<string>) {
-    return ""
+    const crabs = await parseData(data);
+
+    const min = Math.min(...crabs.map(crab => crab.pos));
+    const max = Math.max(...crabs.map(crab => crab.pos));
+
+    let winner = [min, totalFuel2(crabs, min)];
+    for (let pos=min; pos <= max; pos++) {
+        const candidate = [pos, totalFuel2(crabs, pos)];
+        if (candidate[1] < winner[1]) {
+            winner = candidate;
+        };
+    };
+    return winner[1]; 
 }
